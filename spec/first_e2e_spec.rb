16298ed5bdb924e39c0a92a55d4fdb2fa7dab79c
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-08-29 21:43:53 +0900

    2nd e2e test

--- spec/first_e2e_spec.rb
+++ spec/first_e2e_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 RSpec.describe "first e2e test", type: :feature do
   it "sign in" do
     visit "/"
+
+    click_link "Sign up"
+
+    fill_in "user_email", with: "test@example.jp"
+    fill_in "user_password", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+    fill_in "user_name", with: "test"
+    click_button "Sign up"
   end
 end
 
