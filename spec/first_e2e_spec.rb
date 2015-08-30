93b1082843c6b93fdf159fd88accd19c26ffb2ba
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-08-30 11:51:47 +0900

    add verification code

--- spec/first_e2e_spec.rb
+++ spec/first_e2e_spec.rb
@@ -11,6 +11,15 @@ RSpec.describe "first e2e test", type: :feature do
     fill_in "user_password_confirmation", with: "password"
     fill_in "user_name", with: "test"
     click_button "Sign up"
+
+    # メッセージボックスに、登録成功メッセージが表示されていることを検証する
+    within "div.alert-success" do
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+    # サイドバーに、ユーザー名 "test" が表示されていることを検証する
+    within "div.sidebar-nav" do
+      expect(page).to have_content("test")
+    end
   end
 end
 
